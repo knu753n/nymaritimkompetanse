a00b39f8f2b7454090eeaa7dd677b73a03548f90
knu753n <knutsen.b@gmail.com>
2023-02-24 11:11:33 +0100

    lagt til javascript for karusell

--- index.php
+++ index.php
@@ -325,5 +325,5 @@
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="./smooth_scroll.js"></script>
-  <script src="karusell.js" defer></script>
+  <script src="./karusell.js" defer></script>
 </html>
