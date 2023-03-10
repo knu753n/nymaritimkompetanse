ffa435bea1d05f3db0c8f5b8348b59597544be59
knu753n <knutsen.b@gmail.com>
2023-03-10 20:08:48 +0100

    added separate js for articles, updated ms_medstraum

---
+++ karusell-articles.js
@@ -0,0 +1,18 @@
+const buttons = document.querySelectorAll("[data-karusell-button]")
+let newIndex = 0
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const offset = button.dataset.karusellButton === "next" ? 1 : -1
+        const slides = button
+            .closest("[data-karusell]")
+            .querySelector('[data-slides]')
+
+        const activeSlide = slides.querySelector("[data-active]")
+        newIndex = [...slides.children].indexOf(activeSlide) + offset
+        if (newIndex < 0) newIndex = slides.children.length - 1
+        if (newIndex >= slides.children.length) newIndex = 0
+
+        slides.children[newIndex].dataset.active = true
+        delete activeSlide.dataset.active
+    })
+})
