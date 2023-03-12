287fb73603ebf8376ba0efb58a0b896a42e3c59d
knu753n <knutsen.b@gmail.com>
2023-03-12 19:02:37 +0100

    centered text

--- ms_medstraum.php
+++ ms_medstraum.php
@@ -68,7 +68,7 @@ En stor takk til mannskapet som viste oss rundt!
             sizes="50vw"
           >
           <div class="overlay">
-            <p>
+            <p class="center_text">
             MS Medstraum på tur inn til Stavanger hvor den har basen og ladestasjon
             </p>
           </div>
